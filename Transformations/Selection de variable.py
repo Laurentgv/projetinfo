23869cd7bea2e19d8d48f74d082663b035aee7b2
Laurent <laurentjmgov@gmail.com>
2022-05-11 17:11:04 +0200

    modfis select v

--- Transformations/Selection de variable.py
+++ Transformations/Selection de variable.py
@@ -1,3 +1,25 @@
 class selection_variable:
     '''
     '''
+    def __init__(self, tableau):
+        '''
+        '''
+        self.tableau=tableau
+
+    def fun(self):
+        '''
+        ''' 
+        L1=[]
+        for i in range (len(self.tableau)):
+            L1.append((i,self.tableau[i][1]))
+        print(L1)
+        print("Choisissez les variables ques vous souhaitez visualiser.")
+        print("Saisissez N si vous avez fini votre saisie")
+
+
+        a = input()
+        L2=[]
+        while a!="N":
+            L2.append(self.tableau[a])
+            a = input()
+        return L2
