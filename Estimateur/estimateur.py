93af3841255d00aa918520415f1567f93a78f288
Louis <louis.allain@gmail.com>
2022-05-11 16:27:50 +0200

    cool

--- Estimateur/estimateur.py
+++ Estimateur/estimateur.py
@@ -1,6 +1,13 @@
-class Estimateur:
+from abc import ABC, abstractmethod
+
+class Estimateur(ABC):
     '''
     Metrique que l'on veut mesurer/étudier
     
     Je ne sais pas encore trop ce qu'il faut mettre dedans.
     '''
+    def __init__(self) -> None:
+        super().__init__()
+
+    def calcul(self):
+        return 0
