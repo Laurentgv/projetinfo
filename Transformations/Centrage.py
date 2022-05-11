ee04cef7ecfc8c3f461eebcae8bd7e04e2e73e61
Laurent <laurentjmgov@gmail.com>
2022-05-11 16:50:03 +0200

    Modif centrage et norm

--- Transformations/Centrage.py
+++ Transformations/Centrage.py
@@ -4,4 +4,23 @@ class centrage:
     '''
     '''
     def __init__(self, variable):
+        '''
+        '''
         self.variable=variable
+
+    def centrage(self):
+        '''
+        Centrage de la variable
+
+        Attributes
+        ----------
+        variable : list
+
+        Examples
+        --------
+        '''
+        moy = Moyenne.calcul(self.variable)
+
+        for i in range (len(self.variable)):
+            self.variable[i]=self.variable[i] - moy
+            
--- Transformations/Normalisation.py
+++ Transformations/Normalisation.py
@@ -1,6 +1,28 @@
-from ecarttype import EcartType
+from Estimateur.ecarttype import EcartType
 
 class normalisation:
     '''
-    fonction
     '''
+    def __init__(self, variable):
+        '''
+        '''
+        self.variable=variable
+
+    def norma(self):
+        '''
+        Normalisation de la variable
+
+        Attributes
+        ----------
+        variable : list
+
+        Examples
+        --------
+        '''
+        ecart = EcartType.calcul(self.variable)
+        assert(ecart!=0)
+
+        for i in range (len(self.variable)):
+            self.variable[i]=self.variable[i]/ecart
+
+    
