fa5d2f95cd17c1167c637c999e6d84881954d738
Laurent <laurentjmgov@gmail.com>
2022-05-14 11:50:54 +0200

    import

--- Importer/importer.py
+++ Importer/importer.py
@@ -1,7 +1,5 @@
 from abc import ABC, abstractmethod
 
 class Importer(ABC):
-    '''Classe abstraite pour differencier l'import .csv et l'import .json
     '''
-    def __init__(self) -> None:
-        super().__init__()
+    '''
