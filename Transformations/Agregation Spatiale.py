553a3b19ba31c68457421d4316662479525f0cd6
Laurent <laurentjmgov@gmail.com>
2022-05-21 01:07:46 +0200

    yo

--- Transformations/Agregation Spatiale.py
+++ Transformations/Agregation Spatiale.py
@@ -1,69 +1,71 @@
-from gc import get_referents
-from ossaudiodev import SOUND_MIXER_MONITOR
-from sys import getrecursionlimit
-from Estimateur.moyenne import Moyenne
-import Table
+from Table import Table
+from Transformations import Transformations
 
-class Agregation_spatiale(Table):
+class Agregation_spatiale(Transformations):
+
     '''
     '''
-    def __init__(self):
+    def __init__(self,variable,L1,L2,L3,L4):
         '''
         Constructeur
 
+        Attributes
+        ----------
+        variable : str
+            variable sur laquelle on fait l'agrégation
+        L1 : list
+            liste des variables sur laquelle on calcule la moyenne
+        L2 : list
+            liste des variables sur laquelle on somme les valeurs
+        L3 : list 
+            liste des variables qu'on retire de la table
+        L4 : list
+            liste des variables qui ne changent pas sur une agrégation
+        
+        Examples
+        --------
         '''
-        super().__init__()
+        self.variable=variable
+        self.L1=L1
+        self.L2=L2
+        self.L3=L3
+        self.L4=L4
 
-    def selection(self):
+
+    def transfo(self,table:Table):
         '''
-        Permet à l'utilisateur de choisir les régions dont il souahite observer les valeurs.
 
         Description longue
         ------------------
-        L'utilisateur doit saisir les régions qu'il souhaite visualiser.
-        Une nouvelle table est ensuite créée avec les régions que l'utilisateur a choisi.
-        L'utilisateur peut aussi choisir de visualiser les données pour toute la France
+        
 
         Attributes
         ----------
-        variable : list
-            variable dont on souhaite observer les valeurs
-        
-        data : (list of list) or (list of dict)
-            table dont on souhaite observer les valeurs
+    
 
         Examples
         --------
-        >>> a=[]
-        >>> a.fun()
-        [(1, Ille-et-Villaine),
-        (2, Ile-de-France),
-        (3, Normandie)]
-        Quelles régions souhaitez vous visualiser ? Entrez France si vous souhaitez tout visualiser.
-        >>> 1
-        >>> 2
-        
         '''
-        L2=[]
-        L3=[]
-        L1=set(self.var)
-        L1=list(L1)
+        #L3 variables qu'on retire de la table
+        for i in range (len(self.L3)):
+            table.enlev_var(L3[i])
+
+        agreg=list(set(table.extraire_var(self.variable)))
         #Pour séléctionner les valeurs distinctes de la variables
-        for i in range (len(L1)):
-            L2.append((i,L1[i]))
-        print(L2)
-        print("Quelles régions souhaitez vous visualiser ? Entrez France si vous souhaitez tout visualiser.")
-        a=int(input())
-        if a=="France":
-            return self.data
-        else: #peut probablement être optimisé
-            nom=L2[a]
-            for j in range (len(self.var)): 
-                if self.var[j]==nom:
-                    L3.append(self.data[j])
-            return L3
-        #L0 variable par lequel tu veux agréger
-        #L1 liste des variables Moyenne
-        #L2 liste que tu veux sommer
-        #L3 liste que tu veux tej
-        #L4 liste des var qui ne changent pas
+        
+        for i in range (len(agreg)):
+            compt=(self.variable).count(agreg[i])#nombre d'individu pour chaque agregation
+            for a in self.L1:
+                #moyenne
+                ind=(table.var).index(a) #index de la variable
+                m=0
+                for j in range (compt):
+                    m+=table.data[#l'individu][a]
+                m/compt
+            for b in self.L2:
+                #somme
+                ind=(table.var).index(b)
+                for j in range (compt):
+            for c in self.L4:
+                #ne change pas
+                ind=(table.var).index(c)
