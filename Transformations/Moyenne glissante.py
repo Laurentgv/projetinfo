9ce92550c8585c02df13d03321fa7dbac217bfb5
Laurent <laurentjmgov@gmail.com>
2022-05-20 15:35:14 +0200

    moyenne glissante

--- Transformations/Moyenne glissante.py
+++ Transformations/Moyenne glissante.py
@@ -1,12 +1,54 @@
 import Table
+from Estimateur.moyenne import Moyenne
 
 class Moyenne_glissante:
     '''
-    '''
-    def __init__():
-        '''
-        '''
+    Classe des transformations par moyenne glissante
 
-    def calcul():
+    La moyenne glissante modifie une table contenant une seule variable,
+    et renvoie une table contenant dans chaque case la moyenne des "periodes"
+    valeurs suivantes. On appelle "periode" le pas de la moyenne glissante.
+
+    Attributes
+    ----------
+    periode : int
+        Pas de la moyenne glissante
+    '''
+    def __init__(self,variable):
         '''
+        Constructeur
+
+        Attributes
+        ----------
+
+        variable : list
+            Donnees dont on veut obtenir la moyenne glissante
+
         '''
+        self.variable=variable
+
+    def calcul(self,periode):
+        '''
+        Transformation de la liste en moyenne glissante
+        
+        Attributes
+        ----------
+        period : int
+            Pas de la moyenne glissante
+
+        Examples
+        --------
+        >>> a=[24,3,3,3,3,3,3,3,9]
+        >>> a.calcul(3)
+        [10,3,3,3,3,3,5]
+        '''
+        L=[]
+        var=self.variable
+        le=len(var)
+        for i in range (le-periode+1):
+            l=[]
+            for j in range (periode):
+                l.append(L[i+j])
+            m=Moyenne.calcul(l)
+            L.append(m)
+        return L
