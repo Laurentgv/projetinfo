43f1fbb8fc3510e8f931b62786282991c65f86aa
Laurentgv <laurentjmgov@gmail.com>
2022-05-29 20:55:50 +0200

    main et pipeline

---
+++ pipeline.py
@@ -0,0 +1,23 @@
+from table.Table import Table
+from table.transforme import Transforme
+
+from exporter.exporter import Exporter
+from importer.importer import Importer
+
+from estimateur.clustering import Clustering
+from estimateur.ecarttype import EcartType
+from estimateur.moyenne import Moyenne
+from estimateur.somme import Somme
+
+from transformations.Agregation_spatiale import Agregation_spatiale
+from transformations.append import Append
+from transformations.Centrage import Centrage
+from transformations.Fenetrage import Fenetrage
+from transformations.Jointure import Jointure
+from transformations.Moyenne_glissante import Moyenne_glissante
+from transformations.Normalisation import Normalisation
+from transformations.Selection_de_variable import Selection_variable
+
+import meanbyvar
+import outils
+
