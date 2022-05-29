132ca1cac0aef656f8b89be732a1cb276abf2753
Laurentgv <laurentjmgov@gmail.com>
2022-05-29 19:15:20 +0200

    yo

--- Transformations/Agregation_spatiale.py
+++ Transformations/Agregation_spatiale.py
@@ -5,10 +5,9 @@ class Agregation_spatiale(Transformations):
 
     '''
     '''
-    def __init__(self, L1, L2, L3, L4, variable="National"):
+    def __init__(self, L1, L2, L3, L4, variable='National'):
         '''
         Constructeur
-
         Attributes
         ----------
         variable : str
@@ -35,57 +34,60 @@ class Agregation_spatiale(Transformations):
     def transfo(self,table:Table):
         '''
         Permet à l'utilisateur d'obtenir une agrégation spatiale, soit en régions soit national sur la table de données table.
-
         Description longue
         ------------------
         L'utilisateur pourra obtenir une agrégation spatiale sur la variable à agréger de son choix. Si la variable saisie est "National" (choix de base), 
         l'utilisateur obtient une agrégation à l'échelle nationale.
         Les variables sont ensuites agrégées selon si c'est des variables sur lesquelles on veut obtenir : la somme, la moyenne, ou si elle reste 
         la même pour toute l'agrégation ou si elle est tout simplement retirée
-
         Attributes
         ----------
         table : Table
             Table des variables et données
-
         Examples
         --------
+        >>> a=Table([regions,superficie, latitude, temperature],[[IdF,19,4523,28],[IdF,19,32149,14],[Normandie,43,12445,19],[Normandie,43,124133,21]])
+        >>> se=Agregation_spatiale([temperature],[],[latitude],[superficie],"regions")
+        >>> se.transfo(a)
+        Table([regions,superficie, latitude, temperature],[[19,21,IdF],[43,20,Normandie]])
         '''
         def aux(agreg):
-            tab=Table((self.var), [])
-
+            tab2=table.enlev_var(self.variable)
+            tab=Table([tab2.var], [])
 
             for i in range (len(agreg)):
                 L=[] #on crée pour chaque agrégation, la liste de data pour l'agrégation.
-                l_indice=self.l_index(self.variable, agreg[i]) #retourne une liste avec les index des individus dans l'agregation i
+                l_indice=table.l_index(self.variable, agreg[i]) #retourne une liste avec les index des individus dans l'agregation i
                 le=len(l_indice)#nombre d'individu pour chaque agregation
                 for j in range (len(table.var)):
-                    if (table.var)[j] in self.L1:
+                    if (table.var)[j] in self.L1:#moyenne
                         m=0
                         for k in range (le):
                             m+=(table.data)[l_indice[k]][j]
-                        m=m/len(l_indice)
+                        m=m/le
                         L.append(m)
-                    elif (table.var)[j] in self.L2:
+                    elif (table.var)[j] in self.L2:#somme
                         S=0
                         for k in range (le):
                             S+=(table.data)[l_indice[k]][j]
                         L.append(S)
-                    elif (table.var)[j] in self.L4:
+                    elif (table.var)[j] in self.L4:#ne change pas
                         value=(table.data)[l_indice[0]][j] #on prend la première valeur de la variable pour cet agreg car elle ne change pas pour L4
                         L.append(value)
 
-                (tab.data).append(L+agreg[i])
+                L=L+[agreg[i]]
+                (tab.var).append(self.variable)
+                (tab.data).append(L)
 
-            #L3 variables qu'on retire de la table
+            #L3 variables qu'on retire de la table, on doit juste les retirer des variables car on ne les pas ajoutés aux données
             for i in range (len(self.L3)):
-                tab.enlev_var(self.L3[i])
+                (tab.var).pop(self.L3[i])
         
             return tab
         #fin fonction auxiliaire
         
-        if self.variable=="National":
-            agreg=["National"]
+        if self.variable=='National':
+            agreg=['National']
         elif self.variable in table.var:
             agreg=list(set(table.extraire_var(self.variable))) #Pour séléctionner les valeurs distinctes de la variable d'agregation
         else : 
