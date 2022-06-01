06b965257fb783f459281c185bd5e23613a13726
Louis <louis.allain@gmail.com>
2022-06-01 18:48:43 +0200

    nom

--- pipeline.py
+++ pipeline.py
@@ -1,28 +1,27 @@
-from table.Table import Table
+from table.table import Table
 from table.transforme import Transforme
 
 from exporter.exporter import Exporter
 from importer.importer import Importer
 
-from estimateur.clustering import Clustering
 from estimateur.ecarttype import EcartType
 from estimateur.moyenne import Moyenne
 from estimateur.somme import Somme
 
 from transformations.Agregation_spatiale import Agregation_spatiale
 from transformations.append import Append
-from transformations.Centrage import Centrage
-from transformations.Fenetrage import Fenetrage
-from transformations.Jointure import Jointure
-from transformations.Moyenne_glissante import Moyenne_glissante
-from transformations.Normalisation import Normalisation
-from transformations.Selection_de_variable import Selection_variable
-from transformations.Transformations import Transformations
+from transformations.centrage import Centrage
+from transformations.fenetrage import Fenetrage
+from transformations.jointure import Jointure
+from transformations.moyenneglissante import Moyenne_glissante
+from transformations.normalisation import Normalisation
+from transformations.selectionvariable import Selection_variable
+from transformations.transformation import Transformations
 
 import meanbyvar
 import outils
 
-from table import Table
+from table import table
 
 class Pipeline():
     '''
@@ -43,7 +42,7 @@ class Pipeline():
         '''
         self.operations.append(etape)
 
-    def run(self,table) -> Table :
+    def run(self,table) -> table :
         
         for etape in self.operations :
             table = etape(table).transfo()
